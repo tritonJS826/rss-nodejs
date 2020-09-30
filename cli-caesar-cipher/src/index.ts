74e54a9385435aae05e7671c7f142be01edf1d4b
tritonJS826 <triton.68@mail.ru>
2020-09-30 16:26:14 +0300

    feat: base structure

---
+++ cli-caesar-cipher/src/index.ts
@@ -0,0 +1,89 @@
+const { Command } = require('commander'); // (normal include)
+const cipher = new Command();
+
+
+const encodeDecode = (
+  // action: "encode" | "decode" = "encode",
+  // shift: number = 0,
+  // input: string = "",
+  // output: string = "",
+) => {
+  cipher
+    .version('0.0.1')
+    .description('An application for pizzas ordering')
+    .option('-a, --Action <decode|encode>', 'action string: "decode" | "encode"')
+    .option('-s, --Shift <number>', 'shift number: from -26 to 26')
+    .option('-i, --input <string>', 'path to input file')
+    .option('-o, --output <string>', 'path to output file');
+
+  cipher.parse(process.argv);
+
+  // console.log('u start a the program with parameters:');
+  // if (cipher.Action) console.log('  - Action');
+  // if (cipher.Shift) console.log('  - Shift');
+
+  const isActionCorrect = () => {
+    return (cipher.Action === 'decode' || cipher.Action === 'encode');
+  };
+
+  const isShiftCorrect = () => {
+    const isNumber = typeof cipher.Shift === 'number';
+    const isBiggerThanMinus27 = cipher.Shift > -27;
+    const isLessThanMinus27 = cipher.Shift < 27;
+
+    return (isNumber && isLessThanMinus27 || isBiggerThanMinus27)
+  };
+
+  const isInputCorrect = () => {
+    return true;
+  }; // !!!!!!!!!!!!
+  const isOutputCorrect = () => {
+    return true;
+  }; // !!!!!!!!!!!!
+
+  const readNewInput = () => {}; // !!!!!!!!!
+  const readNewOutput = () => {}; // !!!!!!!!!
+
+  const checkInput = () => {
+    if (!isInputCorrect()) {
+      console.error("incorrect input, write new input file path");
+      readNewInput();
+      checkInput();
+    }
+  }; // !!!!!!!!!
+
+  const checkOutput = () => {
+    if (!isOutputCorrect()) {
+      console.error("incorrect output path, write new output file path");
+      readNewOutput();
+      checkOutput();
+    }
+  }; // !!!!!!!!!
+
+  const cipherStart = (): void => {
+    //    pipeline(
+    //      input_stream, // input file stream or stdin stream
+    //      transform_stream, // standard Transform stream or https://github.com/rvagg/through2
+    //      output_stream // output file stream or stdout stream
+    // )
+    //.then(/* success and error callbacks */)
+  }; // !!!!!!!!!
+ // start program
+  if (!isActionCorrect()) {
+    console.error("Error: Action incorrect");
+    process.exit(1);
+  }
+  if (!isShiftCorrect()) {
+    console.error("Error: Shift incorrect");
+    process.exit(1);
+  }
+
+  checkInput();
+  checkOutput();
+
+  cipherStart();
+};
+
+encodeDecode();
+
+console.log("AAA");
